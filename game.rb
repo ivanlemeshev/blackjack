603ec3dbef93bc5b7362e7749fa1c59eadae1f77
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-06 19:56:03 +0600

    game init

--- game.rb
+++ game.rb
@@ -1,10 +1,21 @@
 class Game
   def self.init
     puts 'Welcode to the Blackjack game!'
+
     print 'Please enter your name: '
     player_name = gets.chomp
+
     dealer = Dealer.new('Dealer')
     player = Player.new(player_name)
+
     puts "Hi, #{player.name}!"
+
+    deck = Deck.new
+
+    player.add_cards(deck.deal_cards(2))
+    dealer.add_cards(deck.deal_cards(2))
+
+    p player
+    p dealer
   end
 end
