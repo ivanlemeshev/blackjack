b60f377b591946a5a864f669cd9639e6d9721ab2
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-06 07:20:21 +0600

    include classes in main game file

--- black_jack.rb
+++ black_jack.rb
@@ -1,4 +1,8 @@
 require_relative './deck.rb'
+require_relative './card.rb'
+require_relative './person.rb'
+require_relative './dealer.rb'
+require_relative './player.rb'
 require_relative './game.rb'
 
 Game.init
