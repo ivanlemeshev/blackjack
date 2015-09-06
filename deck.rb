e1e3724eadfdcb901b145336fbb137424033c122
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-06 19:35:22 +0600

    generate deck

--- deck.rb
+++ deck.rb
@@ -1,8 +1,23 @@
 class Deck
-  def initialize(cards)
-    @cards = cards
+  def initialize()
+    @cards = generate_cards
   end
 
   def shuffle
+    @cards.shuffle!
+  end
+
+  private
+
+  def generate_cards
+    cards = []
+    suits = ['♥', '♣', '♦', '♠']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits.each do |suit|
+      values.each do |value|
+        cards << Card.new(suit, value)
+      end
+    end
+    cards.shuffle
   end
 end
