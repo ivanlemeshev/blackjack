15193794c8df480bc9d0d38a6d26de9a675809e7
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-09 08:30:28 +0600

    clear card

--- card.rb
+++ card.rb
@@ -1,23 +1,4 @@
 class Card
-  SUIT_HEARTS   = '♡'
-  SUIT_CLUBS    = '♧'
-  SUIT_DIAMONDS = '♢'
-  SUIT_SPADES   = '♤'
-
-  VALUE_TWO   = '2'
-  VALUE_THREE = '3'
-  VALUE_FOUR  = '4'
-  VALUE_FIVE  = '5'
-  VALUE_SIX   = '6'
-  VALUE_SEVEN = '7'
-  VALUE_EIGHT = '8'
-  VALUE_NINE  = '9'
-  VALUE_TEN   = '10'
-  VALEU_JACK  = 'J'
-  VALUE_QUEEN = 'Q'
-  VALUE_KING  = 'K'
-  VALUE_ACE   = 'A'
-
   attr_reader :suit, :value
 
   def initialize(suit, value)
