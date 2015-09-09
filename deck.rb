0769ff79806c066bbb5d907dab1a896c1811b26a
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-09 08:29:55 +0600

    refactoring of deck

--- deck.rb
+++ deck.rb
@@ -1,10 +1,9 @@
 class Deck
+  CARD_SUITS = ['♡', '♧', '♢', '♤']
+  CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
   def initialize()
     @cards = generate_cards
-  end
-
-  def shuffle
-    @cards.shuffle!
   end
 
   def deal_cards(count)
@@ -15,10 +14,8 @@ class Deck
 
   def generate_cards
     cards = []
-    suits = ['♡', '♧', '♢', '♤']
-    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    suits.each do |suit|
-      values.each do |value|
+    CARD_SUITS.each do |suit|
+      CARD_VALUES.each do |value|
         cards << Card.new(suit, value)
       end
     end
