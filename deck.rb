03707d66ce93b1fe94cc1f47cb191a9b16d584cf
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-13 14:18:41 +0600

    refactoring of deck class

--- deck.rb
+++ deck.rb
@@ -1,8 +1,8 @@
 class Deck
-  CARD_SUITS = ['♡', '♧', '♢', '♤']
-  CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  CARD_SUITS = %w(♡ ♧ ♢ ♤)
+  CARD_VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 
-  def initialize()
+  def initialize
     @cards = generate_cards
   end
 
