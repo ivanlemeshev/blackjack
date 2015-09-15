f6c5212ecf1d1297a57d95f7a73b8a749397cd4f
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-15 07:30:34 +0600

    create card score module to calculate total score

---
+++ card_score.rb
@@ -0,0 +1,36 @@
+module CardScore
+  WIN_SCORE = 21
+  DEFAULT_VALUE = 10
+  ACE_MIN_VALUE = 1
+  ACE_MAX_VALUE = 11
+
+  def score
+    total_score = 0
+    ace_count = 0
+    @cards.each do |card|
+      ace_count += 1 if card.value == 'A'
+      total_score += get_card_score(card)
+    end
+    add_additional_ace_score(total_score, ace_count)
+  end
+
+  protected
+
+  def get_card_score(card)
+    if %w(J Q K).include? card.value
+      DEFAULT_VALUE
+    elsif card.value == 'A'
+      ACE_MIN_VALUE
+    else
+      card.value.to_i
+    end
+  end
+
+  def add_additional_ace_score(total_score, ace_count)
+    ace_count.times do
+      ace_score = ACE_MAX_VALUE - ACE_MIN_VALUE
+      total_score += ace_score if total_score + ace_score <= WIN_SCORE + 5
+    end
+    total_score
+  end
+end
--- user.rb
+++ user.rb
@@ -1,10 +1,8 @@
 class User
-  WIN_SCORE = 21
-  BET_AMOUNT = 10
-  DEFAULT_VALUE = 10
-  ACE_MIN_VALUE = 1
-  ACR_MAX_VALUE = 11
+  include CardScore
+
   INITIALIZE_BALANCE = 100
+  BET_AMOUNT         = 10
 
   attr_reader :name, :cards, :balance
 
@@ -20,29 +18,6 @@ class User
 
   def clear_cards
     @cards = []
-  end
-
-  def score
-    score = 0
-
-    self.cards.each do |card|
-      if ['J', 'Q', 'K'].include? card.value
-        value = DEFAULT_VALUE
-      else
-        value = card.value.to_i
-      end
-      score += value
-    end
-
-    self.cards.each do |card|
-      if card.value == 'A' && score + ACE_MIN_VALUE > WIN_SCORE
-        score += ACE_MIN_VALUE
-      elsif card.value == 'A' && score + ACE_MIN_VALUE < WIN_SCORE
-        score += ACR_MAX_VALUE
-      end
-    end
-
-    score
   end
 
   def make_bet
