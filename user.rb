d61f40383277a0fd493a8f41a5af6d56442e536e
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-15 18:08:01 +0600

    add make_bet adn take_cards to user

--- user.rb
+++ user.rb
@@ -1,11 +1,27 @@
 class User
-  INITIALIZE_BALANCE = 100
+  BET = 10
+  START_BALANCE = 100
 
   attr_reader :name, :cards, :balance
 
   def initialize(name)
     @name = name
-    @balance = INITIALIZE_BALANCE
+    @balance = START_BALANCE
     @cards = []
+  end
+
+  def make_bet
+    @balance -= BET
+    BET
+  end
+
+  def take_cards(cards)
+    @cards.concat(cards) if can_take_cards?
+  end
+
+  protected
+
+  def can_take_cards?
+    @cards.size < 3
   end
 end
